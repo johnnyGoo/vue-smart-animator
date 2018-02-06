dcb95560809568bfc5a778f54a77faec15ef705a
Johnny <157781745@qq.com>
2018-02-06 14:13:45 +0800

    new version 1.1.0

--- README.md
+++ README.md
@@ -13,16 +13,43 @@ npm run build
 
 ### Props:
 ```
+frames:Array [frame,frame,...]
+frame:{styles:Object,configs:Object}
 
+styles CSS object for example
+{ background: 'rgba(100,100,100,.5)', transform: 'translateX(100px) rotateZ(90deg)' }
+
+configs default
+{'duration': '.3s', 'fill-mode': 'forwards', 'timing-function': 'linear'}
+properties:
+delay,direction,duration,iteration-count,name,play-state,timing-function,fill-mode
+
+options:Object
+
+default:{
+                startFrom: 0,//开始帧
+                pauseAt: [],//暂停的帧 需要继续播放调用 resume
+                prefix: true,//是否添加浏览器兼容前缀
+                count: 1,//播放次数
+                clear: false, //是否清除动画数据 
+                applyOnEnd: false, // clear后停留在最后一帧 
+                instant: false //立即播放
+            }
 
 ```
 ### Methods
 ```
-
+play()
+pause()
+resume()
+restart() 
 
 ```
-### Properties
+### Event
 ```
+play
+stop
+next
 
 ```
 
--- src/component.vue
+++ src/component.vue
@@ -8,61 +8,81 @@
 </style>
 
 <script>
-    import {Frame, Queue} from 'animatorjs';
+    function extend(obj) {
+        let length = arguments.length;
+        obj = Object(obj);
+        if (length < 2 || obj == null) return obj;
+        for (let index = 1; index < length; index++) {
+            let source = arguments[index]
+            for (let key in source) {
+                obj[key] = source[key];
+            }
+        }
+        return obj
+    }
+
+    import Frame from './frame';
+    import Queue from './queue';
+
+
     // 注册
     export default {
         name: 'vue-smart-animator',
         // 声明 props
         props: {
-            frames: {default: function () {
+            frames: {
+                default: function () {
                     return []
-                }},
+                }
+            },
             options: {
-                default:function() {
+                default: function () {
                     return {
-                        startFrom: 0,
-                        pauseAt: [],
-                        prefix: false,
-                        count: 1,
-                        clear: true,
-                        applyOnEnd: true,
-                        instant: false
                     }
                 }
             }
         },
         methods: {
             play: function () {
-                let self=this;
+                let self = this;
                 self.$emit('play')
                 this.queue.play().then(function () {
                     self.$emit('next')
-
+                    self.$emit('stop')
                 })
             }, pause: function () {
                 this.queue.pause()
             }, resume: function () {
                 this.queue.resume()
             }, restart: function () {
-                this.queue.restart()
+                 this.play()
             }
         },
         data: function () {
             return {queue: null}
         },
         mounted: function () {
-            let self=this;
-            let frames=this.frames
-            let initFrames=[]
-            for(let i=0;i<frames.length;i++){
-                initFrames.push(new Frame(frames[i].styles,frames[i].configs))
+            let self = this;
+            let frames = this.frames
+            let initFrames = []
+            for (let i = 0; i < frames.length; i++) {
+                initFrames.push(new Frame(frames[i].styles, extend({'duration': '.3s', 'fill-mode': 'forwards', 'timing-function': 'linear'},frames[i].configs)))
             }
+            let options=extend({
+                startFrom: 0,
+                pauseAt: [],
+                prefix: true,
+                count: 1,
+                clear: false,
+                applyOnEnd: false,
+                instant: false
+            }, this.options)
 
-            let instant=this.options.instant
-            this.options.instant=false
-            this.queue = new Queue(this.$el, initFrames, this.options)
-            if(instant){
-               self.play()
+            let instant = options.instant
+            options.instant = false
+            this.queue = new Queue(this.$el, initFrames, options)
+            if (instant) {
+                self.play()
             }
 
         }
